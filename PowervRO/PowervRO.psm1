5878d3662315ed36d3f8ea92eeed8bfdaceca6f9
Craig Gumbley <craiggumbley@gmail.com>
2017-02-02 11:23:32 +0000

    Removes private function

--- PowervRO/PowervRO.psm1
+++ PowervRO/PowervRO.psm1
@@ -1,9 +1,4 @@
-# --- Expose each Public and Private function as part of the module
-foreach ($PrivateScript in Get-ChildItem -Path "$($PSScriptRoot)\Functions\Private\*.ps1" -Recurse -Verbose:$VerbosePreference) {
-
-    . $PrivateScript.FullName
-}
-
+# --- Expose each Public function as part of the module
 foreach ($Publicfunction in Get-ChildItem -Path "$($PSScriptRoot)\Functions\Public\*.ps1" -Recurse -Verbose:$VerbosePreference) {
 
     . $PublicFunction.FullName
