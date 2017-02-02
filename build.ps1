5127c81f85be7198b8d19d118a6bc3825bcbb794
Craig Gumbley <craiggumbley@gmail.com>
2017-02-02 11:17:24 +0000

    improves build support

--- build.ps1
+++ build.ps1
@@ -1,54 +1,94 @@
-<#
+#Requires -Modules Psake, Pester, PSScriptAnalyzer, PlatyPS
+#Requires -Version 5
 
-.SYNOPSIS
-Wrapper for PSake build file
+<#
+    .SYNOPSIS
+    A wrapper script for build.psake.ps1
+
+    .DESCRIPTION
+    This script is a wrapper for the processes defined in build.psake.ps1. For this process to be succesful the following modules are required:
+
+    - Psake
+    - Pester
+    - PSScriptAnalyzer
+    - PlatyPS
+
+    Each task in build.psake.ps1 relies on settings provided in build.settings.ps1
+
+    By default the build task will be executed but it is possible to select individual tasks. See the Task parameter for more information.
+
+    .PARAMETER Task
+    The build task that needs to be executed. The value of this parameter can be:
+
+    - Build
+    - PrepareRelease
+    - Analyze
+    - UpdateModuleManifest
+    - UpdateDocumentation
+    - BumpVersion
+    - Test
+
+    The default value is Build which will execute the following tasks: Analyze, UpdateModuleManifest, UpdateDocumentation
+
+    Chosing release will execute the following tasks: All tasks in Build, Test, BumpVersion.
+
+    The BumpVersion will increment the version of the Module Manifest based on the $BumpVersion setting provided in build.settings.ps1.
+    By default this is patch.
+
+    .PARAMETER Version
+    The part of the version you wish to bump for this release.
+
+    Possible values are:
+
+    - NONE
+    - PATCH
+    - MINOR
+    - MAJOR
+
+    .INPUTS
+    System.String
+
+    .OUTPUTS
+    None
+    
+    .EXAMPLE
+    .\build.ps1
+
+    .Example 
+    .\build.ps1 -Task PrepareRelease
+
+    .Example 
+    .\build.ps1 -Task Analyze
+
+    .Example 
+    .\build.ps1 -Task UpdateModuleManifest
+
+    .Example 
+    .\build.ps1 -Task UpdateDocumentation
+
+    .Example 
+    .\build.ps1 -Task BumpVersion
+
+    .Example 
+    .\build.ps1 -Task Test
 
 #>
 
-[cmdletbinding()]
+[Cmdletbinding()]
 
 Param (
 
-    [Parameter(Mandatory=$false)]
-    [ValidateSet('Build','Release')]
-    [String]$Task = 'Build', 
+    [Parameter()]    
+    [ValidateSet("Build", "PrepareRelease", "Analyze", "UpdateModuleManifest", "UpdateDocumentation", "BumpVersion", "Test")]
+    [String]$Task = "Build",
 
-    [Parameter(Mandatory=$false)]
-    [ValidateSet('Major','Minor','Patch')]
-    [String]$BuildVersion = 'Patch'
-
-)
-
-# --- Build Parameters
-$BaseDirectory = (Resolve-Path -Path .).Path
-$BinDirectory = "$($BaseDirectory)\bin"
-$ModulePath = "$($BaseDirectory)\PowervRO"
-$ModuleManifest = "$($ModulePath)\PowervRO.psd1"
-
-$Parameters = @{
-
-    BaseDirectory = $BaseDirectory
-    BinDirectory = $BinDirectory
-    ModulePath = $ModulePath
-    ModuleManifest = $ModuleManifest
-    BuildVersion = $BuildVersion
-
-}
-
-# --- Build Dependencies
-$Dependencies = @(
-
-    "PSake",
-    "Pester",
-    "PSScriptAnalyzer",
-    "$($BinDirectory)\Update-MKDocsYML.psm1",
-    "$($BinDirectory)\Update-ModuleDocumentation.psm1",
-    "$($BinDirectory)\Update-ModuleManifestVersion.psm1",
-    "$($BinDirectory)\Update-ModuleManifestFunctions.psm1"
+    [Parameter()]
+    [ValidateSet("PATCH", "MINOR", "MAJOR")]
+    [String]$Version
 
 )
-
-$Dependencies | % {Import-Module -Name $_ -Force}
 
 # --- Start Build
-Invoke-psake -buildFile "$($PSScriptRoot)\bin\psake.build.ps1" -taskList $Task -parameters $Parameters -nologo -Verbose:$VerbosePreference
+Invoke-psake -buildFile "$($PSScriptRoot)\build.psake.ps1" -taskList $Task  -parameters @{"Version"=$Version} -nologo -Verbose:$VerbosePreference
+
+exit ( [int]( -not $psake.build_success ) )
