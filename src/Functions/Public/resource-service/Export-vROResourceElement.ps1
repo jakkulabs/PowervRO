b6534ce3dead9b46d75539594436b1b810d724d2
jonathanmedd <jon_medd@hotmail.com>
2018-04-20 18:05:31 +0100

    Added Encoding parameter to fix issue #12

--- src/Functions/Public/resource-service/Export-vROResourceElement.ps1
+++ src/Functions/Public/resource-service/Export-vROResourceElement.ps1
@@ -1,17 +1,22 @@
 ﻿function Export-vROResourceElement {
 <#
     .SYNOPSIS
-    Exports a resource element by its ID. 
-    
+    Exports a resource element by its ID.
+
     .DESCRIPTION
-    Exports a resource element by its ID. 
-    
+    Exports a resource element by its ID.
+
     .PARAMETER Id
     The id of the resource element
 
     .PARAMETER Path
     The path of the exported file. If this parameter is not passed, the resource element
     will be exported to the current working directory.
+
+    .PARAMETER Encoding
+    Encoding of the output file
+    Windows PS Default = UTF8
+    PS Core Default = UTF8NoBOM
 
     .INPUTS
     System.String
@@ -25,6 +30,9 @@
     .EXAMPLE
     Export-vROResourceElement -Id 92768e86-d7bc-400d-bb6d-11e6e10eb133
 
+    .EXAMPLE
+    Export-vROResourceElement -Id 92768e86-d7bc-400d-bb6d-11e6e10eb133 -Encoding Ascii
+
 #>
 [CmdletBinding()][OutputType('System.IO.FileInfo')]
 
@@ -32,31 +40,44 @@
 
     [parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelinebyPropertyName=$true)]
     [ValidateNotNullOrEmpty()]
-    [String]$Id,         
-    
+    [String]$Id,
+
     [parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
-    [String]$Path
+    [String]$Path,
+
+    [parameter(Mandatory=$false)]
+    [ValidateNotNullOrEmpty()]
+    [String]$Encoding
 
     )
 
     begin {
-    
+        if (!$PSBoundParameters.ContainsKey('Encoding')){
+
+            if (!$IsCoreCLR){
+
+                $Encoding = 'UTF8'
+            }
+            else {
+                $Encoding = 'UTF8NoBOM'
+            }
+        }
     }
 
     process {
 
         foreach ($ResourceId in $Id){
 
-            try {    
- 
+            try {
+
                 $URI = "/vco/api/resources/$($ResourceId)"
 
                 $Headers = @{
-                
+
                     "Authorization" = "Basic $($Global:vROConnection.EncodedPassword)";
                     "Accept" =" application/octet-stream";
-                    
+
                 }
 
                 # --- Run vRO REST Request
@@ -74,7 +95,7 @@
                 else {
 
                     Write-Verbose -Message "Path parameter passed."
-                    
+
                     if ($Path.EndsWith("\")) {
 
                         Write-Verbose -Message "Ends with"
@@ -82,16 +103,16 @@
                         $Path = $Path.TrimEnd("\")
 
                     }
-                    
+
                     $FullPath = "$($Path)\$($FileName)"
 
                 }
 
                 Write-Verbose -Message "Exporting resource element to $($FullPath)"
-                $Request.Content | Set-Content -Path $FullPath -Force        
+                $Request.Content | Set-Content -Path $FullPath -Encoding $Encoding -Force
 
                 # --- Output the result
-                Get-ChildItem -Path $FullPath 
+                Get-ChildItem -Path $FullPath
 
             }
             catch [Exception]{
